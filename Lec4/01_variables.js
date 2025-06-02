ff045a737d6ae29e870a09e4f67edf6340e9187a
rajkishore7414 <130090267+rajkishore7414@users.noreply.github.com>
2025-06-02 22:51:52 +0530

    Add files via upload

---
+++ Lec4/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 32345
+let accountEmail = "raj@google.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+
+
+ //accountId = 2  // not allowed
+
+accountEmail = "kishoreai@gmail.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+let accountState
+console.log(accountId)
+
+
+/*
+Prefer not to use to var
+because of issue in block scope and functional scope
+*/
+// console.table([accountId, accountEmail, accountPassword, accountCity])
+
