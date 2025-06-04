216aba86e807026bd6eaa05067c26e99fbc96b52
rajkishore7414 <130090267+rajkishore7414@users.noreply.github.com>
2025-06-04 23:55:37 +0530

    Add files via upload

---
+++ Lec6/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Kishore"
+
+// console.log( typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) // output - number
+// console.log(valueInNumber) //output - NaN
+
+
+// "33" => 33
+// "33abc" => NaN and typeof - number
+// true => 1; false => 0
+
+let isLoggedIn = "kishore"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // output = true
+
+
+
+// 1 => true ; 0 => false
+// "" => false 
+//hitesh => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber) // string - converts into string
+console.log (stringNumber)
