bb6167b5b7776161d93794bc217aa3b2d3177ac6
rajkishore7414 <130090267+rajkishore7414@users.noreply.github.com>
2025-06-05 18:56:09 +0530

    Add files via upload

---
+++ Lec7/03_conversionOperation2.js
@@ -0,0 +1,43 @@
+// Operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "raj"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(ture)
+
+
+// console.log(+true) // output - 1
+// console.log(+""); // output - 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+// link to study
+// https://tc39
+
