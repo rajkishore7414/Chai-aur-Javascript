8f1e9e7b6bb16ecb4974582e4c783c2e08eeae01
rajkishore7414 <130090267+rajkishore7414@users.noreply.github.com>
2025-06-06 23:22:44 +0530

    Add files via upload

---
+++ Lec8/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); //output - false
+// console.log(null == 0); //output - false
+// console.log(null <= 0); //output - true
+
+// console.log(undefined == 0) //output - false
+// console.log(undefined > 0) //output - false
+// console.log(undefined < 0) //output - false
+
+
+// console.log("2" === 2) //output - false //but why?
